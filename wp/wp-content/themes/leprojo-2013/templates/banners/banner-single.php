321c2dbc4ba3a865aff436789379616dce8d5d1b
kartonnade <clouis.allizard@gmail.com>
2013-09-30 12:22:38 +0200

    menu header in progress

--- wp/wp-content/themes/leprojo-2013/templates/banners/banner-single.php
+++ wp/wp-content/themes/leprojo-2013/templates/banners/banner-single.php
@@ -1,8 +1,6 @@
     <header class="banner">
-      <div class="row">
         <figure class="portrait">
           <img src="<?php the_field('photo'); ?>" alt="">
         </figure>
         <h1 class="entry-title"><?php the_title(); ?></h1>
-      </div><!-- row -->
     </header><!-- banner -->
