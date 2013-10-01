47a6712d0e23fae81c6085f92329ab73dad1c170
kartonnade <clouis.allizard@gmail.com>
2013-10-02 00:12:58 +0200

    article Interview is functional

--- wp/wp-content/themes/leprojo-2013/templates/content/content-interview.php
+++ wp/wp-content/themes/leprojo-2013/templates/content/content-interview.php
@@ -2,7 +2,7 @@
   <section id="content" class="content content-itw">
     <div class="row">
       
-      <section class="about large-3 column">
+      <section class="about-itw">
         <h3 class="about-t">A propos de <br/><?php the_field('titre_about'); ?></h3>
         <p class="about-p"><?php the_field('paragraphe_about'); ?></p>
  
@@ -10,18 +10,18 @@
         <ul class="about-links-ul">
           <?php while( has_sub_field('contacts') ) : ?>
             <li class="<?php the_sub_field('info_type_contact'); ?>">
-              <a href="<?php the_sub_field('contact_link'); ?>"><?php the_sub_field('texte_lien_de_contact'); ?></a>
+              <a href="<?php the_sub_field('contact_link'); ?>" target="_blank"><?php the_sub_field('texte_lien_de_contact'); ?></a>
             </li>
           <?php endwhile; ?>
         </ul>
         <?php endif; ?>
 
-        <p class="date">Date de l'interview : <?php  echo locale_date('date_itw'); ?></p>
+        <p class="date">Date de l'interview : <span class="date-info"><?php  echo locale_date('date_itw'); ?></span></p>
       </section>
 
       <!-- introduction -->
       <?php if( get_field('introduction') ): ?>
-        <section class="intro large-9 column">
+        <section class="intro-itw">
           <h3 class="itw-t">Introduction</h3>
           <p><?php the_field('introduction'); ?></p>
         </section>
