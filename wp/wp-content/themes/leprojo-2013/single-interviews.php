910bc749f52f994b9d9ea518c80adf8ade2fe08c
kartonnade <clouis.allizard@gmail.com>
2013-10-02 16:35:50 +0200

    create page single

---
+++ wp/wp-content/themes/leprojo-2013/single-interviews.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+  <?php include(locate_template('templates/loops/loop-single.php')); ?>
+	<?php include(locate_template('templates/nav/navigation-prev-next.php')); ?>
+<?php get_footer(); ?>
