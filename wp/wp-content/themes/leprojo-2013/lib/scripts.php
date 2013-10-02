8a6ff45ee5fb7402c3040aa989ec61e32ec068d3
kartonnade <clouis.allizard@gmail.com>
2013-10-02 20:18:52 +0200

    tries to deactivate useless scripts

--- wp/wp-content/themes/leprojo-2013/lib/scripts.php
+++ wp/wp-content/themes/leprojo-2013/lib/scripts.php
@@ -5,7 +5,7 @@ Enqueue Scripts
 **********************/
 
 // loading modernizr and jquery, and reply script
-function reverie_scripts() {
+function basement_scripts() {
 
     // jQuery is loaded using the same method from HTML5 Boilerplate:
     // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -19,14 +19,14 @@ function reverie_scripts() {
     wp_register_script( 'reverie-modernizr', get_template_directory_uri() . '/assets/js/vendor/custom.modernizr.js', array(), '2.6.2', false );
 
     //google map
-    wp_register_script( 'google-map', get_template_directory_uri() . '/assets/js/vendor/custom.modernizr.js', array(), '2.6.2', false );
+    // wp_register_script( 'google-map', get_template_directory_uri() . '/assets/js/vendor/custom.modernizr.js', array(), '2.6.2', false );
     
     // comment reply script for threaded comments
     if( get_option( 'thread_comments' ) )  { wp_enqueue_script( 'comment-reply' ); }
     
     // adding Foundation scripts file in the footer
-    wp_register_script( 'reverie-js', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '', true );
-    wp_register_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.min.js', array( 'jquery' ), '', true );
+    // wp_register_script( 'reverie-js', get_template_directory_uri() . '/assets/js/scripts.min.js', array( 'jquery' ), '', true );
+    // wp_register_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.min.js', array( 'jquery' ), '', true );
     
     global $is_IE;
     if ($is_IE) {
@@ -34,7 +34,7 @@ function reverie_scripts() {
     }
 
     // enqueue styles and scripts
-    wp_enqueue_script( 'reverie-modernizr' );
+    // wp_enqueue_script( 'reverie-modernizr' );
     /*
     I recommend using a plugin to call jQuery
     using the google cdn. That way it stays cached
@@ -42,11 +42,11 @@ function reverie_scripts() {
     */
     wp_enqueue_script( 'html5shiv' );
     wp_enqueue_script( 'jquery' );    
-    wp_enqueue_script( 'foundation-js' );
-    wp_enqueue_script( 'reverie-js' );
+    // wp_enqueue_script( 'foundation-js' );
+    // wp_enqueue_script( 'reverie-js' );
 }
 
-add_action('wp_enqueue_scripts', 'reverie_scripts', 100);
+add_action('wp_enqueue_scripts', 'basement_scripts', 100);
 
 // http://wordpress.stackexchange.com/a/12450
 function reverie_jquery_local_fallback($src, $handle) {
@@ -82,4 +82,4 @@ if (GOOGLE_ANALYTICS_ID) {
 function embed_gmap() {
     echo "\n\t<script type=\"text/javascript\" src=\"http://maps.googleapis.com/maps/api/js?sensor=false\"></script>\n";
 }
-add_action('reverie_head', 'embed_gmap');
+// add_action('reverie_head', 'embed_gmap');
