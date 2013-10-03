b04bdd2335e57722eaf7ba87468fd4a7deacb5a5
kartonnade <clouis.allizard@gmail.com>
2013-10-03 18:21:05 +0200

    adds facebook in footer

--- wp/wp-content/themes/leprojo-2013/templates/content/content-footer.php
+++ wp/wp-content/themes/leprojo-2013/templates/content/content-footer.php
@@ -1,9 +1,9 @@
 <aside class="engage-w-us">
 	<div class="inside-wrapper">
 		<div class="column large-12">
-				<h5>Cette interview vous a plue et vous en voulez encore ? Consultez <a href="http://leprojo.com/wp/les-archives/"> nos archives</a>.</h5>
+				<h5>Cette interview vous a plue et vous en voulez encore ? Consultez <a href="http://leprojo.com/wp/archives/"> nos archives</a>.</h5>
 				<h5>Pour être certain de ne rater aucune interview, le mieux est de nous suivre sur <a href="#" target="_blank"><span>Twitter : @LeProjo</span></a>.</h5>
-				<h5>Vous pouvez également liker notre <a href="#" target="_blank"><span>page facebook</span></a>.</h5>
+				<h5>Vous pouvez également liker notre <a href="https://www.facebook.com/pages/Le-Projo/212047378972611#" target="_blank"><span>page facebook</span></a>.</h5>
 		</div>
 	</div>
 </aside>
