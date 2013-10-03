c0aa444125ab2322bc1b055addbeae60ccd48ac5
kartonnade <clouis.allizard@gmail.com>
2013-10-03 12:30:25 +0200

    reglages searchform

--- wp/wp-content/themes/leprojo-2013/searchform.php
+++ wp/wp-content/themes/leprojo-2013/searchform.php
@@ -1,10 +1,12 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-	<div class="row collapse">
-		<div class="small-8 columns">
-			<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'reverie'); ?>">
+<div class="columns large-6 medium-8">
+	<form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
+		<div class="row collapse">
+			<div class="small-8 columns">
+				<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'reverie'); ?>">
+			</div>
+			<div class="small-4 columns">
+				<input type="submit" id="searchsubmit" value="<?php _e('Search', 'reverie'); ?>" class="prefix button">
+			</div>
 		</div>
-		<div class="small-4 columns">
-			<input type="submit" id="searchsubmit" value="<?php _e('Search', 'reverie'); ?>" class="prefix button">
-		</div>
-	</div>
-</form>
+	</form>
+</div>
