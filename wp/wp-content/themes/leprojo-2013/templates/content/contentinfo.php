063a7a0c38873a3154f90bb59982303c1f260ae1
kartonnade <clouis.allizard@gmail.com>
2013-10-03 12:20:06 +0200

    probleme with blocked Icons (twitter)

--- wp/wp-content/themes/leprojo-2013/templates/content/contentinfo.php
+++ wp/wp-content/themes/leprojo-2013/templates/content/contentinfo.php
@@ -12,7 +12,7 @@
 	    	<br/>
 
 	      <span class="credits">Réalisé avec 
-	      	<span aria-hidden="true" class="icon-heart-fill icon-alone">
+	      	<span aria-hidden="true" class="micon-heart-fill micon-alone">
 	      		<span aria-hidden="true" class="screen-reader-text">amour</span>
 	      	</span>
 	      par 
@@ -25,9 +25,9 @@
 	  </div>
 	  <section class="contact-us work-w-us-contact">
 	    <ul class="contact-us-list contact-list-footer">
-	      <li class="contact-us-info icon-mail" aria-hidden="true"><a href="mailto:info@leprojo.com" class="email">contact@leprojo.com</a></li>
-	      <li class="contact-us-contact icon-twitter" aria-hidden="true"><a rel="me" href="http://twitter.com/leprojo">@leprojo</a></li>
-	      <li class="contact-us-info icon-facebook" aria-hidden="true"><a rel="me" href="http://facebook.com/leprojo">@leprojo</a></li>
+	      <li class="contact-us-info micon-mail" aria-hidden="true"><a href="mailto:info@leprojo.com" class="email">contact@leprojo.com</a></li>
+	      <li class="contact-us-contact micon-twitter" aria-hidden="true"><a rel="me" href="http://twitter.com/leprojo">@leprojo</a></li>
+	      <li class="contact-us-info micon-facebook" aria-hidden="true"><a rel="me" href="http://facebook.com/leprojo">@leprojo</a></li>
 	    </ul>
 	  </section>	  
 	</div>
