e6a7a073a2b57bcfdeed4057005b15759b362171
kartonnade <clouis.allizard@gmail.com>
2013-10-03 12:31:18 +0200

    404 in progress

--- wp/wp-content/themes/leprojo-2013/404.php
+++ wp/wp-content/themes/leprojo-2013/404.php
@@ -12,15 +12,14 @@
 					<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'reverie'); ?></p>
 				</div>
 				<p><?php _e('Please try the following:', 'reverie'); ?></p>
-				<ul> 
+				<ul class="unstyled"> 
 					<li><?php _e('Check your spelling', 'reverie'); ?></li>
 					<li><?php printf(__('Return to the <a href="%s">home page</a>', 'reverie'), home_url()); ?></li>
 					<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'reverie'); ?></li>
 				</ul>
 			</div>
+			<?php include(locate_template('searchform.php')); ?>
 		</article>
-
 	</div>
-	<?php get_sidebar(); ?>
 		
 <?php get_footer(); ?>
