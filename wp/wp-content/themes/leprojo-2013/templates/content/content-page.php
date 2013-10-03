40edf1d8dea210e79847ff9f5a4e83093c49a68f
kartonnade <clouis.allizard@gmail.com>
2013-10-03 18:21:12 +0200

    cleanup

--- wp/wp-content/themes/leprojo-2013/templates/content/content-page.php
+++ wp/wp-content/themes/leprojo-2013/templates/content/content-page.php
@@ -1,12 +1,10 @@
-  <section id="content" class="content">
+  <section id="content" class="content content-page">
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<div class="entry-content">
-				<header>
-					<h1><?php the_title(); ?></h1>
-				</header>
-				<div class="columns large-8">					
+			<header class="columns small-12">
+				<h1><?php the_title(); ?></h1>
+			</header>
+			<div class="columns medium-9 large-8">
 					<?php the_content(); ?>
-				</div>
 			</div>
 		</article>
   </section>
--- wp/wp-content/themes/leprojo-2013/templates/content/content-single.php
+++ wp/wp-content/themes/leprojo-2013/templates/content/content-single.php
@@ -1,21 +1,17 @@
-  <section id="content" class="content content-itw">
-      <article <?php post_class(); ?>>
-        <section class="intro-itw">
-          <h3 class="itw-t"><?php the_title(); ?></h3>
-        </section>
-        <!--  Row for main content area -->
-        <div class="main-content-wrapper">
-          <section class="main-content">
-            <div class="entry-content">
-              <?php the_content(); ?>
-            </div>
-            <footer>
-              <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
-              <p><?php the_tags(); ?></p>
-            </footer>
-            <?php comments_template(); ?>
-          </section>          
-        </div>
-
-      </article>
+  <section id="content" class="content content-single">
+    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+      <header class="columns small-12">
+        <h1><?php the_title(); ?></h1>
+      </header>
+      <div class="columns medium-9 large-8">
+          <?php the_content(); ?>
+      </div>
+    </article>
+    <footer class="columns medium-9 large-8">
+      <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
+      <p><?php the_tags(); ?></p>
+    </footer>
+    <div class="columns">     
+      <?php comments_template(); ?>
+    </div>
   </section>
