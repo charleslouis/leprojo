bbd34ef05bca46b62170c12f5464dbcc14530569
kartonnade <clouis.allizard@gmail.com>
2013-11-14 18:01:12 +0100

    uploads back from FTP

--- wp/wp-content/themes/leprojo-2013/lib/images.php
+++ wp/wp-content/themes/leprojo-2013/lib/images.php
@@ -4,8 +4,8 @@ if ( function_exists( 'add_image_size' ) ) {
   add_image_size( 'portrait-small-retina', 680, 449, true ); //(cropped)
   add_image_size( 'portrait-medium', 800, 528, true ); //(cropped)
   add_image_size( 'portrait-medium-retina', 1600, 1056, true ); //(cropped)
-  add_image_size( 'portrait-large', 1300, 858, true ); //(cropped)
-  add_image_size( 'portrait-large-retina', 2600, 1716, true ); //(cropped)
+  add_image_size( 'portrait-large', 1300, 600, true ); //(cropped)
+  add_image_size( 'portrait-large-retina', 2600, 1200, true ); //(cropped)
 }
  
 add_filter('image_size_names_choose', 'basement_image_sizes');
