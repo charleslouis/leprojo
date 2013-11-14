a24aa1cf75cbfec4fcc9301c795c31bd7cff7275
kartonnade <clouis.allizard@gmail.com>
2013-11-14 18:44:51 +0100

    inject in-line style in heading

--- wp/wp-content/themes/leprojo-2013/templates/banners/banner-interview.php
+++ wp/wp-content/themes/leprojo-2013/templates/banners/banner-interview.php
@@ -1,4 +1,37 @@
     <?php $photo = get_field('photo'); ?>
+    
+    <?php if( get_field('reglage_du_titre') ): ?>
+      <?php while( has_sub_field('reglage_du_titre') ) :
+       
+            if( get_sub_field('h_alignment') ):
+              $custom_style .= get_sub_field('h_alignment') . ': 0; ';
+            endif;
+            if( get_sub_field('marge_haute') ):
+              $custom_style .= 'padding-top:' . get_sub_field('marge_haute') . '; ';
+            endif;
+            if( get_sub_field('marge_droite') ):
+              $custom_style .= 'padding-right:' . get_sub_field('marge_droite') . '; ';
+            endif;
+            if( get_sub_field('marge_basse') ):
+              $custom_style .= 'padding-bottom:' . get_sub_field('marge_basse') . '; ';
+            endif;
+            if( get_sub_field('marge_gauche') ):
+              $custom_style .= 'padding-left:' . get_sub_field('marge_gauche') . '; ';
+            endif;
+            if( get_sub_field('font_size') ):
+              $custom_style .= 'font-size: ' . get_sub_field('font_size') . '; ';
+            endif;
+            if( get_sub_field('v_alignment') ):
+              if( get_sub_field('v_alignment') == 'middle'):
+              $custom_style .= 'top: 0; padding-top: 25%; ';
+              // $custom_style .= 'margin-top: -1em; ';
+              else :
+              $custom_style .= get_sub_field('v_alignment') .': 0; ';
+              endif;
+            endif;
+
+      endwhile; ?>
+    <?php endif; ?>
     <?php 
     // $photo_sizes = $photo[sizes]; ?>
     <header class="banner">
@@ -10,5 +43,5 @@
           [<?php echo $photo[sizes]['portrait-large-retina']; ?>, (retina)]">
           <figcaption class="photo-credit"><?php the_field('credits_photo'); ?></figcaption>      
         </figure>
-        <h1 class="entry-title" style="text-align: <?php the_field('alignement_du_titre'); ?>;"><?php the_title(); ?></h1>
+        <h1 class="entry-title" style="<?php echo $custom_style; ?>;"><?php the_title(); ?></h1>
     </header><!-- banner -->
