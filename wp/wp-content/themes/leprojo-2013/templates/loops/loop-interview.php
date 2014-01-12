71c22a461f37989098b2e9b2ba6a745a1ccb10c4
kartonnade <clouis.allizard@gmail.com>
2014-01-12 09:34:55 +0100

    merged dev into socialV2

--- wp/wp-content/themes/leprojo-2013/templates/loops/loop-interview.php
+++ wp/wp-content/themes/leprojo-2013/templates/loops/loop-interview.php
@@ -1,16 +1,12 @@
     <div class="main" role="main">
       <article <?php post_class(); ?>>     
         <?php include(locate_template('templates/banners/banner-interview.php')); ?>
-        <!--  Row for main content area -->
-<<<<<<< HEAD
-        <?php include(locate_template('templates/content/content-interview.php')); ?>
-        <?php 
-        include(locate_template('templates/social-sharer.php')); ?>
-=======
-			  <section id="content" class="content content-itw">
-			    <?php include(locate_template('templates/content/a-propos-interview.php')); ?>
-	    	  <?php include(locate_template('templates/content/content-interview.php')); ?>
-	    	</section>
->>>>>>> origin/development
+
+        <section id="content" class="content content-itw">
+          <?php include(locate_template('templates/content/a-propos-interview.php')); ?>
+          <?php include(locate_template('templates/content/content-interview.php')); ?>
+        </section>
+
+        <?php include(locate_template('templates/social-sharer.php')); ?>
         <?php include(locate_template('templates/content/content-footer.php')); ?>
       </article>
