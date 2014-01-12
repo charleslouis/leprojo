03c2ba8ef70f8e34184e364a9508e0b66d55bf46
kartonnade <clouis.allizard@gmail.com>
2014-01-12 08:56:41 +0100

    Overload layout for q-and-a and block-itw in ABOUT

--- wp/wp-content/themes/leprojo-2013/templates/loops/loop-about.php
+++
@@ -1,9 +0,0 @@
-    <div class="main" role="main">
-      <article <?php post_class(); ?>>     
-        <?php include(locate_template('templates/banners/banner-interview.php')); ?>
-        <!--  Row for main content area -->
-			  <section id="content" class="content content-itw">
-			    <?php include(locate_template('templates/content/a-propos-about.php')); ?>
-	    	  <?php include(locate_template('templates/content/content-interview.php')); ?>
-	    	</section>
-      </article>
