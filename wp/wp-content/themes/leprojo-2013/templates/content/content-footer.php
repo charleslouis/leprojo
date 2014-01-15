a461e0ba44c3f1a754b657d8580924d52b5f5fe4
kartonnade <clouis.allizard@gmail.com>
2014-01-15 11:56:35 +0100

    instegrate mailchimp

--- wp/wp-content/themes/leprojo-2013/templates/content/content-footer.php
+++ wp/wp-content/themes/leprojo-2013/templates/content/content-footer.php
@@ -5,5 +5,8 @@
 				<h5>Pour être certain de ne rater aucune interview, le mieux est de nous suivre sur Twitter <a href="http://twitter.com/@LeProjoTweet" target="_blank"><span>@LeProjoTweet</span></a>.</h5>
 				<h5>Si vous préférez, vous pouvez également liker notre <a href="https://www.facebook.com/pages/Le-Projo/212047378972611#" target="_blank"><span>page facebook</span></a>.</h5>
 		</div>
+		<div class="column large-4 left">
+				<?php include(locate_template('templates/content/mailchimp.php')); ?>			
+		</div>
 	</div>
 </aside>
---
+++ wp/wp-content/themes/leprojo-2013/templates/content/mailchimp.php
@@ -0,0 +1,19 @@
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="http://leprojo.us4.list-manage.com/subscribe/post?u=c46fbc0a24d1781028088caeb&amp;id=f4715f1edc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<h5>Inscrivez-vous à notre newsletter</h5>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Addresse mail<span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_c46fbc0a24d1781028088caeb_f4715f1edc" value=""></div>
+	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button small right"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
